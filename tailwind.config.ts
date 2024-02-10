f1bc9858b2045f972cbc64a3995e932d8c9e1891
Luke Cordova <lukethegreat000@gmail.com>
2024-02-09 16:16:17 -0800

    configure tailwind styles.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        black: 'var(--black)',
+        blue: 'var(--blue)',
+        maroon: 'var(--maroon)',
+        purple: 'var(--purple)',
+        grue: 'var(--grue)',
+        'grey-dark': 'var(--grey-dark)',
+        'grey-light': 'var(--grey-light)',
+        'green-neon': 'var(--green-neon',
+        'red-neon': 'var(--red-neon',
+      },
+      fontFamily: {
+        calibri: ['Calibri', 'sans-serif'],
+        segoe: ['Segoe UI', 'sans-serif'],
+        cove: ['CaskaydiaCove Nerd Font', 'monospace'],
       },
     },
   },
