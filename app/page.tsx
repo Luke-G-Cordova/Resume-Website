8eb4650158de0cc1f721d98e59e20b3fed30d08a
Luke Cordova <lukethegreat000@gmail.com>
2024-02-09 20:17:43 -0800

    tabs are working

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
+import Navbar from './components/navbar';
 
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="w-full" style={{ height: '100vh' }}>
+      <Navbar />
+      <div className="w-full h-full bg-black"></div>
+    </div>
+  );
 }
